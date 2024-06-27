9d04f717905b18157524b875950f960637ec24f7
Anouar Kajaj <anouarkajajrosario@gmail.com>
2024-06-27 04:59:40 +0200

    js

---
+++ ejercicio carta/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const cardBack = document.getElementById("card-back");
+    const cardFront = document.getElementById("card-front");
+    if (cardBack && cardFront) {
+        cardBack.addEventListener("mouseover", () => {
+            cardBack.classList.add("hidden");
+            cardFront.classList.remove("hidden");
+            document.body.style.backgroundColor = "rgb(199, 66, 66)";
+        });
+        cardFront.addEventListener("mouseover", () => {
+            cardBack.classList.remove("hidden");
+            cardFront.classList.add("hidden");
+            document.body.style.backgroundColor = "";
+        });
+    }
+});
--- ejercicio piedrapapeltijera js/ejercicio2.js
+++ ejercicio piedrapapeltijera js/ejercicio2.js
@@ -1,57 +1,78 @@
 function gameSettings(userChoice) {
-    let options = ["Piedra", "Papel", "Tijeras"];
-    let computerChoice = options[Math.floor(Math.random() * 3)];
-    let result = "";
+  let options = ["Piedra", "Papel", "Tijeras"];
+  let computerChoice = options[Math.floor(Math.random() * 3)];
+  let result = "";
+  let victorySound = document.getElementById("victorySound");
+  let defeatSound = document.getElementById("defeatSound");
+  let drawSound = document.getElementById("drawSound");
 
+  if (userChoice === computerChoice) {
+    result = "Empate";
+    drawSound.currentTime = 0;
+    drawSound.play();
+    document.body.style.backgroundColor = "";
 
-    if (userChoice === computerChoice) {
-        result = "Empate";
-    } else if (
-        userChoice === "Piedra" && computerChoice === "Tijeras" ||
-        userChoice === "Papel" && computerChoice === "Piedra" ||
-        userChoice === "Tijeras" && computerChoice === "Papel"
-    ) {
-        result = "Has ganado 🏆";
-    } else {
-        result = "Has perdido 😢";
-    }
+    defeatSound.pause();
+    defeatSound.currentTime = 0;
+    victorySound.pause();
+    victorySound.currentTime = 0;
+  } else if (
+    (userChoice === "Piedra" && computerChoice === "Tijeras") ||
+    (userChoice === "Papel" && computerChoice === "Piedra") ||
+    (userChoice === "Tijeras" && computerChoice === "Papel")
+  ) {
+    result = "Has ganado 🏆";
+    victorySound.currentTime = 0;
+    victorySound.play();
+    document.body.style.background = "rgb(107, 229, 115)";
 
+    defeatSound.currentTime = 0;
+    defeatSound.pause();
+    drawSound.currentTime = 0;
+    drawSound.pause();
+  } else {
+    result = "Has perdido 😢";
+    document.getElementById("defeatSound").play();
+    defeatSound.currentTime = 0;
+    defeatSound.play();
+    document.body.style.backgroundColor = "rgb(227, 96, 96)";
 
-    let output = document.getElementById("resultado");
-    output.innerHTML = `
+    victorySound.currentTime = 0;
+    victorySound.pause();
+    drawSound.currentTime = 0;
+    drawSound.pause();
+  }
+
+  let output = document.getElementById("resultado");
+  output.innerHTML = `
     <p>Has escogido: ${userChoice}</p>
     <p>La CPU ha escogido: ${computerChoice}</p>
     <p>${result}</p>
     <button id="jugarDeNuevo" onclick="startGame()">Jugar de nuevo</button>
     `;
 
-    let game = document.getElementById("juego-p");
-    game.style.display = "none";
+  let game = document.getElementById("juego-p");
+  game.style.display = "none";
 }
 
 function startGame() {
+  document.body.style.backgroundColor = "";
 
+  document.getElementById("piedrabtn").addEventListener("click", () => {
+    gameSettings("Piedra");
+  });
+  document.getElementById("papelbtn").addEventListener("click", () => {
+    gameSettings("Papel");
+  });
+  document.getElementById("tijerasbtn").addEventListener("click", () => {
+    gameSettings("Tijeras");
+  });
 
-    document.getElementById("piedrabtn").addEventListener("click", () => {
-        gameSettings("Piedra");
-    });
-    document.getElementById("papelbtn").addEventListener("click", () => {
-        gameSettings("Papel");
-    });
-    document.getElementById("tijerasbtn").addEventListener("click", () => {
-        gameSettings("Tijeras");
-    });
+  let game = document.getElementById("juego-p");
+  game.style.display = "block";
 
-    let game = document.getElementById("juego-p");
-    game.style.display = "block";
-
-    let output = document.getElementById("resultado");
-    output.innerHTML = "";
-    
+  let output = document.getElementById("resultado");
+  output.innerHTML = "";
 }
 
 startGame();
-
-// al darle a un boton el background color cambia(?)
-
-
